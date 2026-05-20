03d2f26a8a9279c26368f9f9bcb41eee4e1a3517
Salva <string.salva@gmail.com>
2026-05-20 12:31:40 +0200

    añadido datos de prueba de jugadores en el main

--- backend/src/main/java/com/altf4/altf4/Main.java
+++ backend/src/main/java/com/altf4/altf4/Main.java
@@ -1,5 +1,5 @@
-
 package com.altf4.altf4;
+
 import java.util.ArrayList;
 
 import org.springframework.boot.CommandLineRunner;
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.altf4.altf4.entity.Equipo;
+import com.altf4.altf4.entity.Jugador;
 import com.altf4.altf4.repository.EquipoRepository;
+import com.altf4.altf4.repository.JugadorRepository;
 
 @SpringBootApplication
 public class Main {
@@ -18,9 +20,13 @@ public class Main {
     }
 
     @Bean
-    CommandLineRunner initData(EquipoRepository repository) {
+    CommandLineRunner initData(
+            EquipoRepository equipoRepository,
+            JugadorRepository jugadorRepository) {
 
         return args -> {
+
+            // EQUIPOS
 
             Equipo e1 = new Equipo(
                     null,
@@ -40,9 +46,57 @@ public class Main {
                     "Sevilla",
                     new ArrayList<>());
 
-            repository.save(e1);
-            repository.save(e2);
-            repository.save(e3);
+            // Guardamos equipos primero
+            e1 = equipoRepository.save(e1);
+            e2 = equipoRepository.save(e2);
+            e3 = equipoRepository.save(e3);
+
+
+            // JUGADORES
+
+            Jugador j1 = new Jugador();
+            j1.setNombre("Jude Bellingham");
+            j1.setDorsal(5);
+            j1.setPosicion("Centrocampista");
+            j1.setEquipo(e1);
+
+            Jugador j2 = new Jugador();
+            j2.setNombre("Vinicius Jr");
+            j2.setDorsal(7);
+            j2.setPosicion("Delantero");
+            j2.setEquipo(e1);
+
+            Jugador j3 = new Jugador();
+            j3.setNombre("Lamine Yamal");
+            j3.setDorsal(19);
+            j3.setPosicion("Extremo");
+            j3.setEquipo(e2);
+
+            Jugador j4 = new Jugador();
+            j4.setNombre("Lewandowski");
+            j4.setDorsal(9);
+            j4.setPosicion("Delantero");
+            j4.setEquipo(e2);
+
+            Jugador j5 = new Jugador();
+            j5.setNombre("Jesús Navas");
+            j5.setDorsal(16);
+            j5.setPosicion("Lateral");
+            j5.setEquipo(e3);
+
+            Jugador j6 = new Jugador();
+            j6.setNombre("Isaac Romero");
+            j6.setDorsal(7);
+            j6.setPosicion("Delantero");
+            j6.setEquipo(e3);
+
+
+            jugadorRepository.save(j1);
+            jugadorRepository.save(j2);
+            jugadorRepository.save(j3);
+            jugadorRepository.save(j4);
+            jugadorRepository.save(j5);
+            jugadorRepository.save(j6);
 
         };
     }
