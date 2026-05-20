d0c12002509531156675818cbef74cfa3975a30f
Salva <string.salva@gmail.com>
2026-05-20 10:05:50 +0200

    Nuestro primer commit

---
+++ src/main/java/com/altf4/altf4/Altf4Application.java
@@ -0,0 +1,13 @@
+package com.altf4.altf4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Altf4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Altf4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/altf4/altf4/Altf4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altf4.altf4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Altf4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
