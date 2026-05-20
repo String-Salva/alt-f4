cd02944ceece8dba48259b7f7f2e6a493d5228f8
Vic <olivenciaibanezvictoria001.alu@stemgranada.com>
2026-05-20 11:20:59 +0200

    Añado Main con datos de prueba

---
+++ backend/src/main/java/com/altf4/altf4/Main.java
@@ -0,0 +1,50 @@
+
+package com.altf4;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.altf4.entity.Equipo;
+import com.altf4.repository.EquipoRepository;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Altf4Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initData(EquipoRepository repository) {
+
+        return args -> {
+
+            Equipo e1 = new Equipo(
+                    null,
+                    "Real Madrid",
+                    "Madrid",
+                    new ArrayList<>());
+
+            Equipo e2 = new Equipo(
+                    null,
+                    "FC Barcelona",
+                    "Barcelona",
+                    new ArrayList<>());
+
+            Equipo e3 = new Equipo(
+                    null,
+                    "Sevilla FC",
+                    "Sevilla",
+                    new ArrayList<>());
+
+            repository.save(e1);
+            repository.save(e2);
+            repository.save(e3);
+
+        };
+    }
+}
