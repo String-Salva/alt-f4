f08a53d6274b5a1e79fe3a055b6400d7e356f5dd
Vic <olivenciaibanezvictoria001.alu@stemgranada.com>
2026-05-20 10:29:54 +0200

    Añado Clase Equipos

---
+++ backend/src/main/java/com/altf4/altf4/Equipo.java
@@ -0,0 +1,68 @@
+
+import java.util.ArrayList;
+
+import javax.annotation.processing.Generated;
+
+@Entity(name = "equipos")
+@Table(name = "EQUIPOS")
+public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column
+    private String ciudad;
+
+    @OneToMany(mappedBy = "equipo", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Jugador> jugadores;
+
+    public Equipo(){}
+
+    public Equipo(Long id, String nombre, String ciudad, List<Jugador> jugadores) {
+        Id = id;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.jugadores = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public List<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(List<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    @Override
+    public String toString() {
+        return Id+ "|| Equipo " + nombre + " de " + ciudad;
+    }
+   
+}
